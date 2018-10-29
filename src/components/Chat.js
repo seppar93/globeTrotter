a308d5cd6c568397a7c40992eceabcb1cabf4b14
Sepehr Parirokh <sepehrparirokh@gmail.com>
2018-10-29 13:54:18 -0400

    Chat debug

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -9,35 +9,18 @@ class Chat extends React.Component {
     super(props);
     this.socket = null;
     this.state = {
-      username: localStorage.getItem("username")
-        ? localStorage.getItem("username")
-        : "",
-      uid: localStorage.getItem("uid")
-        ? localStorage.getItem("uid")
-        : this.generateUID(),
+      username: "sep",
+      id: "1",
       chat_ready: false,
       users: [],
       messages: [],
       message: ""
     };
   }
-
-  render() {
-    return (
-      <div className="chat">
-        {this.state.chat_ready ? (
-          <React.Fragment>
-            <Users users={this.state.users} />
-            <Messages
-              sendMessage={this.sendMessage.bind(this)}
-              messages={this.state.messages}
-            />
-          </React.Fragment>
-        ) : (
-          <EnterChat setUsername={this.setUsername.bind(this)} />
-        )}
-      </div>
-    );
+  componentDidMount() {
+    if (this.state.username.length) {
+      this.iniChat();
+    }
   }
 }
 
