6689b09ec7b1e4d5f113c536f6b77d535a38e1f6
Sepehr Parirokh <sepehrparirokh@gmail.com>
2018-10-29 14:43:00 -0400

    socket functions

--- server.js
+++ server.js
@@ -48,6 +48,57 @@ getUsers = () => {
   });
 };
 
+createSocket = user => {
+  let cur_user = users[user.uid],
+    updated_user = {
+      [user.uid]: Object.assign(cur_user, {
+        sockets: [...cur_user.sockets, user.socket_id]
+      })
+    };
+  users = Object.assign(users, updated_user);
+};
+
+createUser = user => {
+  users = Object.assign(
+    {
+      [user.uid]: {
+        username: user.username,
+        uid: user.uid,
+        sockets: [user.socket_id]
+      }
+    },
+    users
+  );
+};
+
+removeSocket = socket_id => {
+  let uid = "";
+  Object.keys(users).map(function(key) {
+    let sockets = users[key].sockets;
+    if (sockets.indexOf(socket_id) !== -1) {
+      uid = key;
+    }
+  });
+  let user = users[uid];
+  if (user.sockets.length > 1) {
+    // Remove socket only
+    let index = user.sockets.indexOf(socket_id);
+    let updated_user = {
+      [uid]: Object.assign(user, {
+        sockets: user.sockets
+          .slice(0, index)
+          .concat(user.sockets.slice(index + 1))
+      })
+    };
+    users = Object.assign(users, updated_user);
+  } else {
+    // Remove user by key
+    let clone_users = Object.assign({}, users);
+    delete clone_users[uid];
+    users = clone_users;
+  }
+};
+
 io.on("connection", socket => {
   let query = socket.request._query,
     user = {
