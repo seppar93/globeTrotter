206e149469b022c5a6f2c525a97222ed1da8e430
vidhi <panchalvidhi@ymail.com>
2018-11-05 20:06:33 -0500

    map component

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { withGoogleMap, GoogleMap } from 'react-google-maps';
 class Map extends Component {
     render() {
-        const GoogleMap = withGoogleMap(props => (
+        const GoogleMapExample = withGoogleMap(props => (
            <GoogleMap
              defaultCenter = { { lat: 40.756795, lng: -73.954298 } }
              defaultZoom = { 13 }
@@ -11,7 +11,7 @@ class Map extends Component {
         ));
         return(
            <div>
-             <GoogleMap
+             <GoogleMapExample
                containerElement={ <div style={{ height: `500px`, width: '500px' }} /> }
                mapElement={ <div style={{ height: `100%` }} /> }
                />
